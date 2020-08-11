827b88bd036a18f1088a7c50c45c89d254d27542
USERSAD\aperezag <aperezag@everis.com>
2020-08-11 09:05:21 +0200

    Added OCR to swagger

--- src/route/ocr.js
+++ src/route/ocr.js
@@ -4,6 +4,23 @@ var upload = multer({ dest: 'uploads/' })
 module.exports = (app) => {
     const ocrs = require('../controller/ocr.js');
 
+    /**
+     * @swagger
+     * /api/ocr:
+     *  post:
+     *      tags: 
+     *          - OCR
+     *      description: OCR recognize
+     *      consumes:
+     *          - multipart/form-data
+     *      parameters:
+     *          - in: formData
+     *            name: image
+     *            type: file
+     *      responses:
+     *          200:
+     *              description: Ok
+     */
     app.post('/api/ocr', upload.single('image'), ocrs.scan);
 
 }
